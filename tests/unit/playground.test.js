cb99bc181f0b4633062e6590b4d3acc4d5c4f2c3
Rafael Nicolau <rafael.nicolau@sedo.com>
2022-12-19 16:55:31 +0100

    Vitest playground for basic concepts

--- tests/unit/playground.test.js
+++ tests/unit/playground.test.js
@@ -1,7 +1,23 @@
 import { describe, it, expect } from 'vitest';
 
+import { evenOrOdd } from '@/playground';
+
 describe('basic math', () => {
   it('should adds two numbers', () => {
     expect(1 + 1).toBe(2);
+  });
+
+  describe('evenOrOdd', () => {
+    describe('when number is even', () => {
+      it('should return that the number is even', () => {
+        expect(evenOrOdd(2)).toBe('Even');
+      });
+    });
+
+    describe('when number is odd', () => {
+      it('should return that the number is odd', () => {
+        expect(evenOrOdd(3)).toBe('Odd');
+      });
+    });
   });
 });
