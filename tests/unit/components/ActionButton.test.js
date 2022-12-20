6d99f7e79a18e5b78eac514e3664b4baec2f90d6
Rafael Nicolau <rafael.nicolau@sedo.com>
2022-12-20 18:03:32 +0100

    Action Button with proper style validation

---
+++ tests/unit/components/ActionButton.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/vue';
+
+import ActionButton from '@/components/ActionButton.vue';
+
+describe('ActionButton', () => {
+  it('should render text', () => {
+    render(ActionButton, {
+      props: {
+        text: 'test me',
+        type: 'primary',
+      },
+    });
+
+    const button = screen.getByRole('button', { name: /test me/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should apply one of the several styles to the button', () => {
+    render(ActionButton, {
+      props: {
+        text: 'test me',
+        type: 'primary',
+      },
+    });
+
+    const button = screen.getByRole('button', { name: /test me/i });
+    expect(button).toHaveClass('primary');
+  });
+});
