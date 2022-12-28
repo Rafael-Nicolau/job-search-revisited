4243926f6dc1ee42420bd7520e0ba4e77b82394f
Rafael Nicolau <rafael.nicolau@sedo.com>
2022-12-28 12:47:46 +0100

    add SpotLight for emitting slots props

---
+++ src/components/JobSearch/SpotLight.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li v-for="spotlight in spotlights" :key="spotlight.id">
+      <slot :spotlight="spotlight"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'SpotLight',
+  data() {
+    return {
+      spotlights: [],
+    };
+  },
+  async mounted() {
+    const response = await axios.get(import.meta.env.VITE_APP_API_SPOT);
+    this.spotlights = response.data;
+  },
+};
+</script>
