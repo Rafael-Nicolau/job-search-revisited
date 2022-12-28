cc5c7170a2b8aa828e1347c336a702fe047e6712
Rafael Nicolau <rafael.nicolau@sedo.com>
2022-12-28 12:27:05 +0100

    add HeaderContainer with tests

--- src/components/navigation/MainNav.vue
+++ src/components/navigation/MainNav.vue
@@ -62,7 +62,7 @@ export default {
     //This is a method inside this component's object
     return {
       menuItems: [
-        { text: 'Teams', url: '/' },
+        { text: 'Teams', url: '/teams' },
         { text: 'Locations', url: '/' },
         { text: 'Life at Nicolau Careers', url: '/' },
         { text: 'How we hire', url: '/' },
